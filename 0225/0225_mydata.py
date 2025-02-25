aec7a465631c80a7c8d75583699f3f23420d9b7f
ES401N48\USER <411422259@m365.fju.edu.tw>
2025-02-25 11:15:05 +0800

    my meta data

---
+++ 0225/0225_mydata.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 25 10:34:56 2025
+
+Author: 蕭羽芳 統資三乙 411422259
+Topic: create a csv file
+"""
+
+import pandas as pd
+mydata = pd.read_csv("My.csv",encoding="utf-8")
+#老師是CP950
